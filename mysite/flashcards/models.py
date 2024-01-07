63705275b3a8c9d718407e6d3ebd46d3e6b7b77e
Kirill F <kirill.olegovich31@gmail.com>
2024-01-08 00:53:42 +0300

    a few models created

--- mysite/flashcards/models.py
+++ mysite/flashcards/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("publication date")
+
+
+class User(models.Model):
+    user_name = models.CharField(max_lengt=64)
+
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=500)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
+
+
+class Votes(models.Model):
+    raise NotImplementedError
+
+
+
+
