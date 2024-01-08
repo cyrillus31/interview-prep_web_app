edd4506a18b748b61ec649998d1c9606aee26075
Kirill F <kirill.olegovich31@gmail.com>
2024-01-08 15:26:19 +0300

    createing first index view and template

--- mysite/flashcards/models.py
+++ mysite/flashcards/models.py
@@ -6,19 +6,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField("publication date")
 
 
-class User(models.Model):
-    user_name = models.CharField(max_lengt=64)
-
-
 class Answer(models.Model):
     answer_text = models.CharField(max_length=500)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
-
-
-class Votes(models.Model):
-    raise NotImplementedError
-
-
-
-
