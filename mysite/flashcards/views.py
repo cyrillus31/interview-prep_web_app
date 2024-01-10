8c0747c3ca5da6fc9ef845c583b7cac9e958bbd4
Kirill F <kirill.olegovich31@gmail.com>
2024-01-11 01:26:23 +0300

    created basic single flashcard view fucntion

--- mysite/flashcards/views.py
+++ mysite/flashcards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 
 
 from . import models
@@ -7,6 +7,16 @@ from . import models
 def index(request):
     context = {
             "username": "Cyrillus",
-            "questions": [q.question_text for q in models.Question.objects.all()]
+            "questions": [(q.id, q.question_text) for q in models.Question.objects.all()]
             }
     return render(request, "index.html", context)
+
+
+def flashcards(request, question_id: int):
+    question = get_object_or_404(models.Question, pk=question_id)
+    context = {
+            "question": question.question_text,
+            }
+    return render(request, "flashcard.html", context)
+
+
