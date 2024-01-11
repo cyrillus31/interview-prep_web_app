8b50116b019126cc3c747ec4802eb6df2bd741af
Kirill F <kirill.olegovich31@gmail.com>
2024-01-11 15:20:37 +0300

    new migrations

--- mysite/flashcards/models.py
+++ mysite/flashcards/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.utils import choices, timezone
 
+
+LANGUAGE_CHOICE = {
+        "EN": "English",
+        "RU": "Russian",
+        }
+        
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("publication date")
+    pub_date = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=30, blank=True, null=True)
+    language = models.CharField(max_length=3, choices=LANGUAGE_CHOICE, null=True, blank=True)
+    votes = models.IntegerField(default=0)
 
 
     def __str__(self):
@@ -14,6 +24,9 @@ class Answer(models.Model):
     answer_text = models.CharField(max_length=500)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=30, blank=True, null=True)
+    language = models.CharField(max_length=3, choices=LANGUAGE_CHOICE, null=True, blank=True)
 
 
     def __str__(self):
